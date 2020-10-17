9af9f0ef5f2424920c39e3980b7bf06270e7b328
Jose Aznar <josea@kanto.studio>
2020-10-17 17:55:26 -0500

    Fix minir bug

--- src/questions/questions.module.ts
+++ src/questions/questions.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { QuestionOption } from './question-option.entity';
+import { QuestionsOptionService } from './question-option.service';
 import { Question } from './question.entity';
 import { QuestionsController } from './questions.controller';
 import { QuestionsService } from './questions.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Question, QuestionOption])],
-  providers: [QuestionsService],
+  providers: [QuestionsService, QuestionsOptionService],
   controllers: [QuestionsController],
   exports: [TypeOrmModule]
 })
