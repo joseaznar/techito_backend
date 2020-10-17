727dd1ebd3a893f7195ccdd10ebab02c68081e13
Jose Aznar <josea@kanto.studio>
2020-10-17 18:27:36 -0500

    Fix minor bug

--- src/questions/question-option.entity.ts
+++ src/questions/question-option.entity.ts
@@ -15,11 +15,6 @@ export class QuestionOption {
     @Column()
     content: string;
 
-    @ApiProperty({
-        description: "The question.",
-        required: true,
-        type: Question,
-    })
     @ManyToOne(() => Question, question => question.options)
     question: Question;
 }
--- src/questions/question.entity.ts
+++ src/questions/question.entity.ts
@@ -3,40 +3,40 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 import { QuestionOption } from './question-option.entity';
 
 export enum QuestionType {
-    MultipleChoice = 'MultipleChoice',
-    Open = 'Open',
+  MultipleChoice = 'MultipleChoice',
+  Open = 'Open',
 }
 
 @Entity()
 export class Question {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @ApiProperty({
-        description: "The question content.",
-        example: 'What is your name?',
-        required: true,
-    })
-    @Column()
-    content: string;
+  @ApiProperty({
+    description: 'The question content.',
+    example: 'What is your name?',
+    required: true,
+  })
+  @Column()
+  content: string;
 
-    @ApiProperty({
-        description: "The question's type.",
-        enum: QuestionType,
-        required: true,
-    })
-    @Column()
-    type: QuestionType;
+  @ApiProperty({
+    description: "The question's type.",
+    enum: QuestionType,
+    required: true,
+  })
+  @Column()
+  type: QuestionType;
 
-    @ApiProperty({
-        description: `The options to answer this questions.`,
-        example: [
-            '0-500',
-            '500-1000',
-            '1000+',
-        ],
-        required: false,
-    })
-    @OneToMany(type => QuestionOption, questionsOption => questionsOption.question)
-    options?: QuestionOption[];
+  @ApiProperty({
+    description: `The options to answer this questions.`,
+    example: ['0-500', '500-1000', '1000+'],
+    required: false,
+  })
+  @OneToMany(
+    type => QuestionOption,
+    questionsOption => questionsOption.question,
+    { cascade: true, eager: true, nullable: true },
+  )
+  options?: QuestionOption[];
 }
